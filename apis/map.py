a10196e1a650cbf9acba2b5473af8dea1af78bbd
Anh Bui <metwaroi31@gmail.com>
2024-11-05 23:34:13 +0700

    feat: integrate map API

---
+++ apis/map.py
@@ -0,0 +1,32 @@
+import aiohttp
+
+MAP_URL = 'http://localhost:8080/points'
+
+async def send_post_request(json_of_point):
+    json_data = {
+        "osmId": "11994579471",
+        "osmName": "Masjidka isbahaysiga جامع التضامن الإسلامي",
+        "code": 3300,
+        "fclass": "muslim",
+        "geom": {
+            "srid": "4326",
+            "longitude": json_of_point['Longitude'],
+            "latitude": json_of_point['lat']
+        },
+        "name": str(json_of_point['shop_name']),
+        "address": str(json_of_point['address']),
+        "description": str(json_of_point['product']),
+        "status": "mở cửa",
+        "opening_hours": "Mở lúc 7:30 Th 2 - Th 6",
+        "rating": 4.6,
+        "reviews": 13,
+        "url": str(json_of_point['file_name']),
+        "file": str(json_of_point['file_name']),
+        "logo": "http://example.com/logo.png",   
+        "businessType": str(json_of_point['category'])
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(MAP_URL, json=json_data) as response:
+            print("Status:", response.status)
+            print("Response:", await response.text())
+
--- main.py
+++ main.py
@@ -15,6 +15,16 @@ from image_utils.read_metadata import (
     get_exif_data,
     write_metadata_to_csv
 )
+from apis.map import send_post_request
+from vintern_llava.models_for_prompt import ShopInfo
+from vintern_llava.LLMs import ImageOCRLLM
+from langchain.output_parsers import PydanticOutputParser
+import torch
+import glob
+import asyncio
+
+LLM_MODEL = ImageOCRLLM()
+
 model = load_model(
     "groundingdino/config/GroundingDINO_SwinT_OGC.py",
     "weights/groundingdino_swint_ogc.pth"
@@ -61,3 +71,32 @@ while True:
             logits=logits,
             phrases=phrases
         )
+        pydantic_parser = PydanticOutputParser(pydantic_object=ShopInfo)
+        format_instructions = pydantic_parser.get_format_instructions()
+        question = '<image>\n' + format_instructions
+        for image_frame in store_sign_images:
+            pixel_values = from_tensor_to_pixels(image_frame, max_num=6).to(torch.bfloat16).cuda()
+            json_of_point = {}
+            json_of_point['file_name'] = image_exif_data['File Name']
+            json_of_point['lat'] = float(image_exif_data['Latitude'])
+            json_of_point['Longitude'] = float(image_exif_data['Longitude'])
+            json_of_point['created_date'] = image_exif_data['Date Created']
+            json_of_point['modified_date'] = image_exif_data['Date Modified']
+            # json_of_point['shop_name'] = None
+            # json_of_point['address'] = None
+            try:
+                shop_json = LLM_MODEL.prompt_model(pixel_values, question)
+                json_of_point['shop_name'] = shop_json['shop_name']
+                json_of_point['address'] = shop_json['address']
+                json_of_point['category'] = shop_json['category']
+                json_of_point['product'] = shop_json['product']
+                print (shop_json)
+                asyncio.run(send_post_request(json_of_point))
+            except Exception as error:
+                print (str(error))
+                print ("bad images")
+            JSON_to_csv.append(json_of_point)
+            
+    write_metadata_to_csv(JSON_to_csv, "report.csv")
+    break
+    
